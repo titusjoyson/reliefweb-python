e9265cf0446f3b15dbb46680a947de13264f17c6
Titus Joyson <tj.joyson@gmail.com>
2025-07-26 11:20:49 +0530

    Added description for setup

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup, find_packages
+import os
+
+with open(os.path.join(os.path.dirname(__file__), "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name='reliefweb',
     version='0.1.0',
     description='Python client library for the ReliefWeb API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Titus Joyson',
     author_email='tj.joyson@gmail.com',
     packages=find_packages(),
